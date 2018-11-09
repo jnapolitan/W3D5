9262cab37d68b9feb18661fa12d0b4e493777074
Julian Napolitan <julian.napolitan@gmail.com>
2018-11-09 15:28:18 -0800

    Create basic methods for Active Record Lite

---
+++ ActiveRecordLite/lib/01_sql_object.rb
@@ -0,0 +1,104 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        '#{table_name}'
+    SQL
+    @columns = cols.first.map(&:to_sym)
+  end
+
+  def self.finalize!
+    columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |val|
+        self.attributes[col] = val
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name || self.name.tableize
+  end
+
+  def self.all
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        '#{table_name}'.*
+      FROM
+        '#{table_name}'
+    SQL
+    parse_all(results)
+  end
+
+  def self.parse_all(results)
+    results.map { |result| new(result) }
+  end
+
+  def self.find(id)
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+
+    return nil if result.empty?
+    new(result.first)
+  end
+
+  def initialize(params = {})
+    # debugger
+    params.each do |attr_name, val|
+      attr_name = attr_name.to_sym
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
+      send("#{attr_name}=", val)
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    self.class.columns.map { |col| self.send(col) }
+  end
+
+  def insert
+    col_names = self.class.columns.join(', ')
+    question_marks = (["?"] * col_names.split(', ').length).join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
